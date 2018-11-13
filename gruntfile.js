42eed3ef223af2e78b4ccc6eb358368b87ff9aa3
yun0121 <1445236951@qq.com>
2018-11-13 16:29:44 +0800

    配置文件

--- gruntfile.js
+++ gruntfile.js
@@ -31,20 +31,33 @@ module.exports = function(grunt){
 		// 	}
 		// },
 		clean:{
-			dest:['dist/*']
+			dest:['dist/*','sample/js/*']
 		},
 		jshint:{
 			test:['src/js/*.js'],
 			options:{
 				jshintrc:'.jshintrc'
 			}
-		}
+		},
+		copy:{
+			js:{expand:true,cwd:'dist/js/',src:'*.min.js',dest:'sample/js/'}
+		},
+		// replace:{
+		// 	example:{
+		// 		src:['sample/js/index.html'],
+		// 		overwrite:true,
+		// 		replacements:[{
+		// 			form:/wipe-*.min.js/g,
+		// 		}]
+		// 	}
+		// }
 	});
 	//告诉grunt需要使用插件
 	  grunt.loadNpmTasks("grunt-contrib-uglify");
 	  grunt.loadNpmTasks("grunt-contrib-cssmin");
 	  grunt.loadNpmTasks("grunt-contrib-clean");
 	  grunt.loadNpmTasks("grunt-contrib-jshint");
+	  grunt.loadNpmTasks("grunt-contrib-copy");
 	//告诉grunt当我们输入grunt命令后需要做些什么,有先后顺序
-	grunt.registerTask('default',['jshint','clean','uglify']);
+	grunt.registerTask('default',['jshint','clean','uglify','copy']);
 };
